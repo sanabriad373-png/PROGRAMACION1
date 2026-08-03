c54f1cedb7b19a9c05f9787b8dc0c447e1c332ef
sanabriad373-png <sanabriad373@gmail.com>
2026-08-03 17:39:24 -0600

    Avance proyecto

---
+++ interessimpl/src/main/java/com/mycompany/interessimpl/Interessimpl.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.interessimpl;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Daniel Sanabria
+/* Interés simple Escriba un programa que calcule el interés simple de un préstamo. El sistema
+debe solicitar el capital inicial, la tasa de interés anual (en formato de número entero) y el tiempo
+en años. Aplique la fórmula (Capital * Tasa * Tiempo) / 100  y muestre el interés generado
+ */
+public class Interessimpl {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+       
+        Scanner input = new Scanner(System.in);
+      
+      double capitainicial= 0;
+      double tasadeinteres=0;
+      double tiempoenanos=0;
+      int capital='0';
+      int tasa='0';
+      int tiempo= '0';
+      
+        System.out.println("Ingrese su capitalinicial:");
+        capitainicial=input.nextDouble();
+        
+        System.out.println("Ingrese la tasadeinteres:");
+        tasadeinteres=input.nextInt();
+        
+        System.out.println("Ingrese el tiempoenanos:");
+        tiempoenanos=input.nextDouble();
+        
+        input.nextLine();//Limpieza del buffer 
+        
+        
+        
+        
+        tasadeinteres=capital * tasa * tiempo / 100;
+    
+          System.out.println("interes generado es: " + tasadeinteres);
+    }
+}
