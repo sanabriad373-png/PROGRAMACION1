b95bbf6fb5fb474712c08588642a6ee55c5be369
sanabriad373-png <sanabriad373@gmail.com>
2026-08-10 19:30:16 -0600

    Avance proyectojj

---
+++ cicloFlor/src/cicloflor/CicloFlor.java
@@ -0,0 +1,77 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package cicloflor;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Daniel Sanabria
+ */
+public class CicloFlor {
+
+    /**
+     * ++ va incremando en uno en uno
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        /*
+        1. variable de control= valor inicial
+        2.condicion=limite
+        3.Mdificador de la v.c= incremento/decremento
+        */
+     
+        System.out.println("CICLO For 1");
+        for (int i = 0; i < 10; i++) {
+            System.out.printf("Ciclo %d\n",i);
+        }    
+         System.out.println("CICLO For 2");
+            for (int i = 2; i < 10; i+=2) {
+                //i=i + 5;  i+=5
+                System.out.printf("Ciclo %d\n",i);
+           }
+            Scanner input = new Scanner(System.in); 
+            
+            final  int  NUMEROVOTANTES=20;
+           int numVarones= 0;
+           int numMujeres= 0;
+           char seleccion='1' ;               
+          
+
+            
+            for (int i = 0; i < NUMEROVOTANTES; i++) {
+                System.out.println("Genero de Votantes");
+                System.out.println("V: Varon / M: MUjer");
+                seleccion= input.next().toUpperCase().charAt(0);
+                
+                switch (seleccion) {
+                    case 'V':
+                        System.out.println("varon entrando");
+                        System.out.println("a ala cabina");
+                        numVarones++;
+                        break;
+                        
+                     case 'm':
+                         System.out.println("Mujer entrando");
+                        System.out.println("a ala cabina");
+                        numMujeres++;
+                         
+                        
+                        break;
+                        
+                    default :
+                        System.out.println("Error de ingreso");
+                        
+                }
+        }
+            }
+          
+          
+        }
+     
+     
+    
+    
+
